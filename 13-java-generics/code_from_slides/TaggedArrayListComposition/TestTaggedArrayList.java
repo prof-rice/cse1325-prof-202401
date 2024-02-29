22bcb57146013dccd69ad98ed0fae73dd6f78e92
Professor Rice <george.rice@uta.edu>
2024-02-29 15:25:48 -0600

    Lecture 13 fix TaggedArrayList mdi test program

--- 13-java-generics/code_from_slides/TaggedArrayListComposition/TestTaggedArrayList.java
+++ 13-java-generics/code_from_slides/TaggedArrayListComposition/TestTaggedArrayList.java
@@ -1,56 +1,77 @@
 import java.util.Scanner;
 
 public class TestTaggedArrayList {
-    public static String menu() {
-        return "Main Menu\n"
-             + "=========\n\n"
-             + "1) Add a String\n"
-             + "2) Add an Integer\n"
-             + "3) Add a Coordinate\n"
-             + "0) Exit\n";
-    }
-             
     public static void main(String[] args) {
-        TaggedArrayList<String> strings = new TaggedArrayList<>();
-        TaggedArrayList<Integer> ints = new TaggedArrayList<>();
-        TaggedArrayList<Coordinate> coords = new TaggedArrayList<>();
-        
-        Scanner in = new Scanner(System.in);
-        int option = 99;
-        String discard = "";
-        
-        while (option != 0) {
-            System.out.print(menu() + "\nSelection? ");
-            option = in.nextInt(); discard = in.nextLine();
-            if (option == 0) {
-            } else if (option == 1) {
-                System.out.print("String? ");
-                strings.add(in.nextLine());
-            } else if (option == 2) {
-                System.out.print("Integer? ");
-                ints.add(in.nextInt()); discard = in.nextLine();
-            } else if (option == 3) {
-                System.out.print("Coordinate (x y z)? ");
-                coords.add(new Coordinate(
-                    in.nextDouble(), 
-                    in.nextDouble(), 
-                    in.nextDouble())); discard = in.nextLine();
-            } else {
-                System.err.println("Invalid option: " + option);
-            }   
-        }
-        
-        System.out.println("\nStrings\n=======\n");
-        for(int i=0; i<strings.size(); ++i) 
-            System.out.println("" + strings.when(i) + " '" + strings.get(i) + "'");
-        
-        System.out.println("\nIntegers\n========\n");
-        for(int i=0; i<ints.size(); ++i) 
-            System.out.println("" + ints.when(i) + " '" + ints.get(i) + "'");
-        
-        System.out.println("\nCoordinates\n===========\n");
-        for(int i=0; i<coords.size(); ++i) 
-            System.out.println("" + coords.when(i) + " '" + coords.get(i) + "'");
-        
+        (new TestTaggedArrayList()).mdi();
     }
+    
+    public TestTaggedArrayList() {
+        menu.addMenuItem(new MenuItem("Exit", () -> exit()));
+        menu.addMenuItem(new MenuItem("Add a String", () -> addString()));
+        menu.addMenuItem(new MenuItem("Add an Integer", () -> addInteger()));
+        menu.addMenuItem(new MenuItem("Add a Coordinate", () -> addCoordinate()));
+    }
+    
+    public void mdi() {
+        while(running) {
+            String input = "";
+            try {
+                System.out.print("\n".repeat(255) + "MAIN MENU\n=========\n\n"
+                              + menu + data() + "\nSelection? ");
+                input = in.nextLine();
+                menu.run(Integer.parseInt(input));
+            } catch(Exception e) {
+                System.err.println("#### Invalid input: " + input);
+            }
+        }
+    }
+    private String data() {
+        StringBuilder sb = new StringBuilder();
+        if(strings.size() > 0) {
+            sb.append("\nStrings\n=======\n");
+            for(int i=0; i<strings.size(); ++i) 
+                sb.append("" + strings.when(i) + " '" + strings.get(i) + "'");
+            sb.append('\n');
+        }
+        if(ints.size() > 0) {
+            sb.append("\nIntegers\n========\n");
+            for(int i=0; i<ints.size(); ++i) 
+                sb.append("" + ints.when(i) + " '" + ints.get(i) + "'");
+            sb.append('\n');
+        }
+        if(coords.size() > 0) {
+            sb.append("\nCoordinates\n===========\n");
+            for(int i=0; i<coords.size(); ++i) 
+                sb.append("" + coords.when(i) + " '" + coords.get(i) + "'");
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
+    private void exit() {
+        running = false;
+    }
+    private void addString() {
+        System.out.print("String? ");
+        strings.add(in.nextLine());
+    }
+    private void addInteger() {
+        System.out.print("Integer? ");
+        ints.add(in.nextInt()); discard = in.nextLine();
+    }
+    private void addCoordinate() {
+        System.out.print("Coordinate (x y z)? ");
+        coords.add(new Coordinate(
+            in.nextDouble(), 
+            in.nextDouble(), 
+            in.nextDouble())); discard = in.nextLine();
+    }
+    
+    private TaggedArrayList<String> strings = new TaggedArrayList<>();
+    private TaggedArrayList<Integer> ints = new TaggedArrayList<>();
+    private TaggedArrayList<Coordinate> coords = new TaggedArrayList<>();
+    
+    private Menu menu = new Menu();
+    private Scanner in = new Scanner(System.in);
+    private boolean running = true;
+    private String discard;
 }
