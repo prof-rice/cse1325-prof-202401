653b703a374d9fb8fa3d488a1176b8ec06ccf338
Professor Rice <george.rice@uta.edu>
2024-03-07 07:52:03 -0600

    Lecture 15 fix bug in Search.java

--- 15-java-class-library/code_from_slides/Search.java
+++ 15-java-class-library/code_from_slides/Search.java
@@ -7,13 +7,15 @@ public class Search {
     public static void main(String[] args) {
         List<String> list = Arrays.asList(args); // convert array to List
         Collections.sort(list); // binary searches only work on sorted lists
+        for(String s : list) System.out.print(s + " ");
+        System.out.println();
         Scanner in = new Scanner(System.in);
         while(true) {
             System.out.print("Search key: ");
             String key = in.nextLine(); if(key.isEmpty()) break;
             int index = Collections.binarySearch(list, key);
-            if(index >= 0) System.out.print(key + " found at index " + index + ". ");
-            else System.out.print(key + " not found. ");
+            if(index >= 0) System.out.println(key + " found at index " + index + ".\n");
+            else System.out.println(key + " not found.\n");
         }        
     }
 }
